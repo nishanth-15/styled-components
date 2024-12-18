b13c73b584a41868bb532fca8e63664826ef0c60
nishanth-15 <142199070+nishanth-15@users.noreply.github.com>
2024-12-18 15:52:37 +0530

    added styled button

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/components/button";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center gap-4">
+      <Button>Button</Button>
+    </main>
+  );
+}
---
+++ src/components/button/button.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+  border-radius: 4px ; 
+`;

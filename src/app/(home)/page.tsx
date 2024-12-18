429cf2921114e387e5b50347b68cfd4929bbe448
nishanth-15 <142199070+nishanth-15@users.noreply.github.com>
2024-12-18 16:00:00 +0530

    added props to the button styled component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,9 @@ import { Button } from "@/components/button";
 
 export default function Home() {
   return (
-    <main className="h-screen flex items-center justify-center gap-4">
-      <Button>Button</Button>
+    <main className="h-screen flex items-center justify-center gap-6 flex-col">
+      <Button>Solid Button</Button>
+      <Button variant="outline">Outline Button</Button>
     </main>
   );
 }
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { ButtonHTMLAttributes } from "react";
 import styled from "styled-components";
 
-export const Button = styled.button`
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "outline" | "solid"; // Add all possible values for `variant`
+};
+
+export const Button = styled.button<ButtonProps>`
   border: 2px solid #4caf50;
-  background-color: #4caf50;
-  color: white;
+  background-color: ${(props) =>
+    props.variant === "outline" ? "#FFF" : "#4caf50"};
+  color: ${(props) => (props.variant === "outline" ? "#4caf50" : "#FFF")};
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   cursor: pointer;
   transition: 0.5s all ease-out;
-  border-radius: 4px ; 
-`;
+  border-radius: 4px;
+`; 
